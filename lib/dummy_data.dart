43ac2f6c51c9dab66fcec130f8933fb395725c17
SafeHeaded <m.patryk@outlook.com>
2021-12-23 17:32:28 +0100

    basic setup

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    'c1',
+    'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    'c2',
+    'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    'c3',
+    'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    'c4',
+    'German',
+    color: Colors.amber,
+  ),
+  Category(
+    'c5',
+    'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    'c6',
+    'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    'c7',
+    'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    'c8',
+    'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    'c9',
+    'French',
+    color: Colors.pink,
+  ),
+  Category(
+    'c10',
+    'Summer',
+    color: Colors.teal,
+  ),
+];
